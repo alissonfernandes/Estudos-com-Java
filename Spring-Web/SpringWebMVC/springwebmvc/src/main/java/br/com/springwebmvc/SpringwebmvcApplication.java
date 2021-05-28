7fb30b1823f4da2c96829d66ccf058fc11210f73
Alisson Fernandes <alissonfernandes200@gmail.com>
2021-05-27 22:09:01 -0300

    SpringWebMVC
    Entendendo o Spring Web MVC.

---
+++ Spring-Web/SpringWebMVC/springwebmvc/src/main/java/br/com/springwebmvc/SpringwebmvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.springwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebmvcApplication.class, args);
+	}
+
+}
