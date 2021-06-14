c8e58d1f24b77b1b2abb132e339efa8e415c2a44
Alisson Fernandes <alissonfernandes200@gmail.com>
2021-06-14 19:05:27 -0300

    API-Rest-update
    Corrigindo erro do método Post e alterando os endereços de requisição.

--- Spring-Web/AplicacaoREST/apirest/src/main/java/br/com/apirest/rest/PessoaResource.java
+++ Spring-Web/AplicacaoREST/apirest/src/main/java/br/com/apirest/rest/PessoaResource.java
@@ -1,12 +1,12 @@
 package br.com.apirest.rest;
 
-import br.com.apirest.exception.PessoaNotFoundException;
 import br.com.apirest.model.Pessoa;
 import br.com.apirest.repository.PessoaRepository;
 import java.util.List;
 import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,12 +20,12 @@ public class PessoaResource {
     @Autowired
     private PessoaRepository pessoaRepository;
     
-    @GetMapping("/pessoa")
+    @GetMapping("/api/pessoa")
     public List<Pessoa> getAllPessoa(){
         return pessoaRepository.findAll();
     }
     
-    @GetMapping("/pessoa/{id}")
+    @GetMapping("/api/pessoa/{id}")
     public ResponseEntity getPessoa(@PathVariable("id") Long id){
         final Optional<Pessoa> pessoa = pessoaRepository.findById(id);
         
@@ -37,7 +37,7 @@ public class PessoaResource {
     }
     
     @PostMapping("/api/pessoa")
-    public Pessoa createPessoa(@Valid @RequestBody Pessoa pessoa){
-        return pessoaRepository.save(pessoa);
+    public ResponseEntity<Pessoa> createPessoa(@Valid @RequestBody Pessoa pessoa){
+        return new ResponseEntity<Pessoa>(pessoaRepository.save(pessoa), HttpStatus.CREATED);
     }
 }
